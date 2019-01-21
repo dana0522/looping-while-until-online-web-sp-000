0ac4081bfa79f063d8e7cf76bb898872caa303fb
dana kim <dana0522@live.com>
2019-01-21 01:57:09 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,8 +2,9 @@ def using_until
   levitation_force = 6
   
   until levitation_force == 10
-    puts "Wingardium Leviosa"
+    puts levitation_force
     levitation_force=+1
+    break
   end
 end
 
